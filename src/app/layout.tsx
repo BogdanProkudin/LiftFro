41736e75c974906926325dec444c3d5514e09352
Bohdan <prokudinbogdan50@gmail.com>
2026-02-28 17:27:41 +0100

    desc2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  console.log("HELLO EVERYBODY");
+  console.log("HELLO EVERYBODY22");
 
   return (
     <html lang="en">
