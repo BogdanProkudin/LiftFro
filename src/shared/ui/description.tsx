4b8e574d09ba40e5debe036d47b7606a3bbaa191
HrafovMaksym <mgrafov06@gmail.com>
2026-03-01 17:52:21 +0100

    fixes

--- src/shared/ui/description.tsx
+++ src/shared/ui/description.tsx
@@ -2,18 +2,16 @@ import React from "react";
 
 const Description = ({
   description,
-  size,
+
   styles,
 }: {
   description: string;
-  size?: number;
+
   styles?: string;
 }) => {
-  const textSize = size ? `${size}px` : "16px";
   return (
     <p
-      style={{ fontSize: textSize }}
-      className={`${styles} text-md text-[var(--description-text-color)]`}
+      className={`${styles ? styles : "text-[16px]"}  text-[var(--description-text-color)]`}
     >
       {description}
     </p>
--- src/shared/ui/title.tsx
+++ src/shared/ui/title.tsx
@@ -2,19 +2,16 @@ import React from "react";
 
 const Title = ({
   title,
-  size,
+
   styles,
 }: {
   title: string;
-  size?: number;
+
   styles?: string;
 }) => {
-  const textSize = size ? `${size}px` : "24px";
-
   return (
     <h1
-      style={{ fontSize: textSize }}
-      className={`${styles} font-bold text-[var(--default-text-color)]`}
+      className={`${styles ? styles : "text-[24px]"} font-bold text-[var(--default-text-color)]`}
     >
       {title}
     </h1>
