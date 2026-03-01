24a52341cdf851d621e3ec8282be8c96c678d577
HrafovMaksym <mgrafov06@gmail.com>
2026-03-01 13:13:25 +0100

    init ap

---
+++ src/app/providers/index.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from "react";
+
+import { IntlProvider } from "./intl-provider";
+import { ModeProvider } from "./theme-provider";
+import { ReduxProvider } from "./redux-provider";
+import { ToastProvider } from "./toast-provider";
+
+export const MainProvider = ({ children }: { children: ReactNode }) => {
+  return (
+    <ModeProvider>
+      <ReduxProvider>
+        <IntlProvider>{children}</IntlProvider>
+      </ReduxProvider>
+      <ToastProvider></ToastProvider>
+    </ModeProvider>
+  );
+};
